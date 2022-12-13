3e330141f69cc28f73dd742d807004fc5c0406f8
George Agachi <georgeagache@ymail.com>
2022-12-14 00:43:11 +0200

    Second Commit (Making a branch)

--- src/main/java/com/siit/webapp/CatalogueService.java
+++ src/main/java/com/siit/webapp/CatalogueService.java
@@ -17,17 +17,15 @@ public class CatalogueService {
 
     private final StudentsRepository studentsRepository;
     public String createStudentCatalogue(){
-        List<Student> studentListToBeProcessed = studentsRepository.getStudents();
+        List<Student> studentListToBeProcessed = studentsRepository.getStudentList(studentsRepository.readCSVFile());
         String result = "";
-
         for (Student student: studentListToBeProcessed) {
             result = result.concat(student.getFirstName()).concat(" ").concat(student.getLastName().concat("<br></br>"));
         }
-
         return result;
     }
 
-    public Map<Student, Double> calculateAverageSortGrades (HashMap<Student, List<Integer>> studentWithGrades) {
+    public Map<Student, Double> calculateAverageSortGrades (Map<Student, List<Integer>> studentWithGrades) {
         HashMap<Student, Double> averageStudentGradeMap = new HashMap<>();
         for (Student St : studentWithGrades.keySet()) {
             OptionalDouble gradesAverage = studentWithGrades.get(St).stream()
@@ -35,23 +33,19 @@ public class CatalogueService {
                     .average();
             averageStudentGradeMap.put(St, gradesAverage.orElse(0));
         }
-
         Map<Student, Double> averageDescendingSortedStudentGradeMap = averageStudentGradeMap.entrySet().stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-
         return averageDescendingSortedStudentGradeMap;
     }
 
     public String createStudentRank(){
-        Map<Student, Double> studentListToBeDisplayed = calculateAverageSortGrades(studentsRepository.addGradesToStudents(studentsRepository.getStudents()));
-
+        Map<Student, Double> studentListToBeDisplayed = calculateAverageSortGrades(studentsRepository.getStudentsWithGrades(studentsRepository.readCSVFile()));
         String result = "";
         DecimalFormat df = new DecimalFormat("0.00");
         for(Map.Entry<Student, Double> entry : studentListToBeDisplayed.entrySet()) {
             result = result.concat(entry.getKey().getFirstName()).concat(" ").concat(entry.getKey().getLastName()).concat(" ").concat(df.format(entry.getValue())).concat("<br></br>");
         }
-
         return result;
     }
 
